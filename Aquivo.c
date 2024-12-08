2b7aada2c0818a80381244ae6d28e76183efd217
Mateus De Jesus Santos <mateussant0sc0osta@outlook.com>
2024-12-08 19:41:20 -0300

    Testando
    Variaveis globais dentro de uma struct

---
+++ Aquivo.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <locale.h>
+typedef struct {
+    int op;
+} VariaveisGlobais;
+VariaveisGlobais vrg = {0};
+void questoes(VariaveisGlobais *vrg);
+FILE *pArquivo;
+
+int main(){
+    pArquivo = fopen ("TreinandoArquivoEmC.txt", "w");
+    if(pArquivo == NULL){
+        printf("Nao foi possivel abrir o arquivo");
+        exit(1);
+    }
+    else{
+        printf("Arquivo aberto com sucesso\n");
+    }
+    printf("Ola digite a quaestão que deseija acessar:");
+    questoes(&vrg);
+
+    if(fclose(pArquivo) == 0){
+            printf("Arquivo fechado com secesso");
+        }
+    else{
+        printf("Erro ao fechar o arquivo");
+    }
+    return 0;
+}
+void questoes(VariaveisGlobais * vrg){
+    printf("\n 1\n>>");
+    scanf("%d",&vrg->op);
+    fprintf(pArquivo,"Opção: %d\n",vrg->op);
+    return;
+}
